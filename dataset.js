5111060a3caa20190197477bb7ba2f168e5805ad
Arulzha <47468344+Arulzha@users.noreply.github.com>
2022-07-20 12:21:57 +0700

    Add files via upload

---
+++ dataset.js
@@ -0,0 +1,19 @@
+var items = [
+    ['',
+    'ADBTC', 
+    'PTC', 
+    'CRYPTOCOINS | PAYEER', 
+    '500 SATOSHIS', 
+    'FAUCETPAY | DIRECT WALLET',
+    'https://r.adbtc.top/1556287', 
+    'READ'],
+    ['',
+    'FAUCETPAY', 
+    'MICROWALLET', 
+    'CRYPTOCOINS', 
+    'VARIOUS',
+    'DIRECT WALLET', 
+    'https://faucetpay.io/?r=543287', 
+    'READ'],
+];
+ 
